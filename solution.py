e31af199aa16119c2db58ca82d2b485649ca6c29
Yathartsalil <kelkaryatharth1@gmail.com>
2025-12-01 15:24:38 +0400

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,15 @@
+# Asking the user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculations
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2  # assuming num2 is not zero
+
+# Output
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Division:", division)
